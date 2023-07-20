74593556f2b49cc490ee8d2015e844a73390b8ce
Nicholas-Mercado <nicholasamerc@gmail.com>
2023-07-20 11:57:46 -0700

    add logic for less then three args

--- scripts/positional_script.sh
+++ scripts/positional_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+if [[ $# -ne 3 ]]; then
+    echo "You didn't enter exactly 3 parameters"
+    echo "USAGE: $0 <arg1> <arg2> <arg3>"
+fi
+    echo "my name is $1"
+    echo "my home directory is $2"
+    echo "my favorite color is $3"
+    echo "the 10th arg is  ${10:-NULL}"
+    echo "the 11th arg is  ${11:-NULL}"
 
-echo "my name is $1"
-echo "my home directory is $2"
-echo "my favorite color is $3"
-echo "the 10th arg is  ${10}"
-echo "the 11th arg is  ${11}"
-
-echo "You inputed $# args"
+    echo "You inputed $# args"
