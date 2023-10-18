2d5af74ce575b055c93686e1f330391bd3699b5f
Nicholas-Mercado <nicholasamerc@gmail.com>
2023-10-18 12:48:27 -0700

    checks for mem usage

---
+++ scripts/memory_logger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR=$HOME/projects/bash_course/files/performance
+
+if [ -d "$DIR" ]; then
+  echo "$DIR exists "
+else
+        echo "$DIR Does not exists"
+        echo "Creating directory now"
+fi
+
+
+# If statement
+    #CHeck $HOME/nicho/projects/bash_course/files/performance exists 
+    # If not create
+    # Echo if exists
+
+# Append output of free command to $HOME/performance/memory.log use <ps> cmd
