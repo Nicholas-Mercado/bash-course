1b4699a5f1b642bd21e35446950e00d300bf026c
Nicholas-Mercado <nicholasamerc@gmail.com>
2023-10-19 13:05:22 -0700

    case statement for each city by country of origin

--- scripts/case_stat.sh
+++ scripts/case_stat.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Select statement 
-    #use follwoing cities 
-    #Tokyo, London, Los Angeles, Moscow, Dubai, Manchester, New York, Paris Bangalore, Johannesburg, Istanbul, Milan, Abu Dhabi, Pune, Nairobi, Berlin, Karachi
-PS3="Pick a city and I will tell you what country it is in"
-select city in Tokyo London "Los Angeles" Moscow Dubai Manchester "New York" Paris;
+PS3="Pick a city and I will tell you what country it is in > "
+select city in Tokyo London "Los Angeles" Manchester "New York" Paris;
 do
-echo "$city"
+case "$city" in
+    Tokyo ) echo "Tokyo is in Japan";;
+    London | Manchester ) echo "$city is in the UK";;
+    "Los Angeles" | "New York" ) echo "$city is in Freedom Land";;
+    Paris ) echo "Paris is in France";;
+esac
+
 break
 done
 # setup case statement within select statment
