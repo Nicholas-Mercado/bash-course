39562baaf21793d92814b7fd07a367f6dafc740e
Nicholas-Mercado <nicholasamerc@gmail.com>
2023-11-09 13:57:58 -0800

    moved for saftey

---
+++ scripts/dir_test/dir_create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Step 1: Then, you will need to create a read-while loop which will process a file and create all of the directories.
+
+#Step 2: Your script should allow the user to enter the path to the folders_to_create.txt file as a command line argument.ls
+
